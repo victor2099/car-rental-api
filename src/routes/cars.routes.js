3b4b6b7e4d658b8163ff4d6eeec255e633ca48ef
FredAbod <fredrickbolutife@gmail.com>
2025-07-03 20:40:21 +0100

    added the endpoint to rent cars

--- src/routes/cars.routes.js
+++ src/routes/cars.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { addCar, editCar, deleteCar, getAllCars, searchCars } = require('../controller/admin.controller');
 const {isAuthenticated} = require('../middlewares/isAuth');
+const { rentCar } = require('../controller/rental.controller');
 
 router.get('/get-cars', getAllCars);
 router.get('/search-cars', searchCars);
@@ -9,5 +10,7 @@ router.post('/add-car',isAuthenticated, addCar);
 router.put('/edit-car/:carId',isAuthenticated, editCar);
 router.delete('/delete-car/:carId',isAuthenticated, deleteCar);
 
+
+router.post('/rent-car/:carId', isAuthenticated, rentCar)
 
 module.exports = router;
